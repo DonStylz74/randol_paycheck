76daf9487202e341c25debf75a38e0625e63f863
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-05-19 00:44:06 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 return {
     model = `a_f_y_business_02`,
-    coords = vec4(-250.9869, -924.3577, 32.3433, 308.5219), -- Default LSJC Office.
+    coords = vec4(-236.3389, -921.5898, 31.3433, 335.5942), -- Default LSJC Office.
 }
